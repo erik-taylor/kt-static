3c46f1f46ed738400f0a7bcbdf1409390ae6c240
Erik Taylor <eriktaylor25@gmail.com>
2018-07-14 22:00:46 -0700

    billing page, nav changes, image and content updates

--- assets/hbs_templates/body.js
+++ assets/hbs_templates/body.js
@@ -3,7 +3,7 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"compi
 
   return "<!DOCTYPE html>\n<html>\n	<head>\n		<title>"
     + container.escapeExpression(((helper = (helper = helpers.page_title || (depth0 != null ? depth0.page_title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"page_title","hash":{},"data":data}) : helper)))
-    + "</title>\n		<link rel=\"stylesheet\" href=\"/assets/css/main.css\">\n\n		<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400\" rel=\"stylesheet\">\n\n	</head>\n	<body>\n\n"
+    + "</title>\n		<link rel=\"stylesheet\" href=\"/assets/css/main.css\">\n\n		<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400\" rel=\"stylesheet\">\n		<link rel=\"shortcut icon\" href=\"/assets/img/favicon.ico\">\n	</head>\n	<body>\n\n"
     + ((stack1 = container.invokePartial(partials["@partial-block"],depth0,{"name":"@partial-block","data":data,"indent":"\t\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "		\n"
     + ((stack1 = container.invokePartial(partials.footer,depth0,{"name":"footer","data":data,"indent":"\t\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
